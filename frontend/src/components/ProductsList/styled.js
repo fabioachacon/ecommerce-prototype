75619f345254e31a589b775eaf33195110d568e6
Fabio <fabioac71@gmail.com>
2021-03-21 18:18:59 -0300

    Buun!

--- frontend/src/components/ProductsList/styled.js
+++ frontend/src/components/ProductsList/styled.js
@@ -5,7 +5,7 @@ export const ProductList = styled(motion.div)`
     display: grid;
     grid-template-columns: repeat(3, minmax(300px, 1fr));
     grid-gap: 35px;
-
+    margin: 10px 0;
     @media screen and (max-width: 700px) {
         grid-template-columns: repeat(1, minmax(300px, 1fr));
     }
